d4eaa71c804c6bca4b615ba8ab35a81774e7d295
eduardo cezar <eduardocezar565143@gmail.com>
2021-12-12 18:08:15 -0300

    passando informação do uso da memoria em Gb para o email

--- monitoramento_memoria.sh
+++ monitoramento_memoria.sh
@@ -1,13 +1,14 @@
+
 #!/bin/bash
 
 memoria_total=$(free | grep -i mem | awk '{ print $2 }')
 memoria_consumida=$(free | grep -i mem | awk '{ print $3 }')
 relacao_memoria_atual_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total *100" | awk -F. '{ print $1 }')
 
-if [ $relacao_memoria_atual_total -gt 50 ]
+if [ $relacao_memoria_atual_total -gt (seu limiar) ]
 then
-mail -s "Consumo de memória acima do limite" adm.mutllidae@gmail.com<<del
-O consumo de memoria esta acima do limite que foi especificado. Atualmente o consumo e de ?
+mail -s "Consumo de memória acima do limite" [seu e-mail]<<del
+O consumo de memória está acima do limite que foi especificado. Atualmente o consumo e de $(free -h | grep -i mem | awk '{ print $3 }')
 del
 
 fi
